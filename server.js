d32af5373127cdfa33be1ce514fa01fe7e97bd86
Jesse <jessecdenier@gmail.com>
2024-03-14 23:13:02 -0500

    Got printEmployees working and looking nice but its a lot of code for something that may be simple.

--- server.js
+++ server.js
@@ -22,8 +22,8 @@ const db = mysql.createConnection(
   console.log(`Connected to the employee_tracker_db database.`)
 );
 
-// Requests to see all departments.
-async function viewdepartments() {
+// Prints all departments in a nicely formatted box.
+async function printDepartments() {
   db.query("SELECT id, name FROM department", function (err, results) {
     if (err) {
       console.error(err);
@@ -41,8 +41,8 @@ async function viewdepartments() {
   });
 }
 
-// Shows all roles in a nicely formatted box.
-async function viewroles() {
+// Prints all roles in a nicely formatted box.
+async function printRoles() {
   db.query(
     "SELECT role.id, role.title, role.salary, department.name AS department FROM role JOIN department ON role.department_id = department.id",
     function (err, results) {
@@ -67,12 +67,115 @@ async function viewroles() {
   );
 }
 
+async function printEmployees() {
+  db.query(
+    "SELECT employee.id, CONCAT(employee.first_name, ' ', employee.last_name) as name, CONCAT(manager.first_name, ' ', manager.last_name) AS manager, role.title AS title, role.salary AS salary, department.name AS department FROM employee LEFT JOIN employee AS manager ON employee.manager_id = manager.id JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id",
+    function (err, results) {
+      if (err) {
+        console.error(err);
+        return;
+      }
+
+      // Find maximum lengths for each column
+      const maxLengths = {
+        id: 0,
+        name: 0,
+        manager: 0,
+        title: 0,
+        salary: 0,
+        department: 0,
+      };
+      results.forEach((row) => {
+        Object.keys(row).forEach((key) => {
+          maxLengths[key] = Math.max(maxLengths[key], String(row[key]).length);
+        });
+      });
+
+      // Console log header
+      console.log(
+        "+" +
+          "-".repeat(maxLengths.id + 2) +
+          "+" +
+          "-".repeat(maxLengths.name + 2) +
+          "+" +
+          "-".repeat(maxLengths.manager + 2) +
+          "+" +
+          "-".repeat(maxLengths.title + 2) +
+          "+" +
+          "-".repeat(maxLengths.salary + 2) +
+          "+" +
+          "-".repeat(maxLengths.department + 2) +
+          "+"
+      );
+      console.log(
+        `| id | name${" ".repeat(maxLengths.name - 4)} | manager${" ".repeat(
+          maxLengths.manager - 7
+        )} | title${" ".repeat(maxLengths.title - 5)} | salary${" ".repeat(
+          maxLengths.salary - 6
+        )} | department${" ".repeat(maxLengths.department - 10)} |`
+      );
+      console.log(
+        "+" +
+          "-".repeat(maxLengths.id + 2) +
+          "+" +
+          "-".repeat(maxLengths.name + 2) +
+          "+" +
+          "-".repeat(maxLengths.manager + 2) +
+          "+" +
+          "-".repeat(maxLengths.title + 2) +
+          "+" +
+          "-".repeat(maxLengths.salary + 2) +
+          "+" +
+          "-".repeat(maxLengths.department + 2) +
+          "+"
+      );
+
+      // Console log rows
+      results.forEach((row) => {
+        const { id, name, manager, title, salary, department } = row;
+        console.log(
+          `| ${id.toString().padStart(2)} | ${name.padEnd(maxLengths.name)} | ${
+            manager
+              ? manager.padEnd(maxLengths.manager)
+              : " ".padEnd(maxLengths.manager)
+          } | ${title.padEnd(maxLengths.title)} | ${salary
+            .toString()
+            .padStart(maxLengths.salary)} | ${department.padEnd(
+            maxLengths.department
+          )} |`
+        );
+      });
+
+      // Console log footer
+      console.log(
+        "+" +
+          "-".repeat(maxLengths.id + 2) +
+          "+" +
+          "-".repeat(maxLengths.name + 2) +
+          "+" +
+          "-".repeat(maxLengths.manager + 2) +
+          "+" +
+          "-".repeat(maxLengths.title + 2) +
+          "+" +
+          "-".repeat(maxLengths.salary + 2) +
+          "+" +
+          "-".repeat(maxLengths.department + 2) +
+          "+"
+      );
+
+      reinit();
+    }
+  );
+}
+
 // Selects a function based on answers to the prompt.
 function pickaction(answers) {
   if (answers.action === "View all departments") {
-    viewdepartments();
+    printDepartments();
   } else if (answers.action === "View all roles") {
-    viewroles();
+    printRoles();
+  } else if (answers.action === "View all employees") {
+    printEmployees();
   }
 }
 
@@ -91,13 +194,13 @@ function reinit() {
         init();
       } else {
         //TODO: Change this so it automatically exits for you.
-        console.log("Okay press 'control + c' to exit.");
+        console.log("Press 'control + c' to exit.");
       }
     });
 }
 
 // Asks the user for choices regarding logo creation and then runs all logo creation functions.
-function init() {
+async function init() {
   inquirer
     .prompt([
       {
@@ -122,7 +225,8 @@ function init() {
 
 init();
 
-//TODO: Add notation
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+//TODO: Add notation.
+//TODO: Stop blocking this out but keep it from messing up init.
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
