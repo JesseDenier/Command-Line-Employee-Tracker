d1672ec6e07c046e436822351aeb9e14767edecc
Jesse <jessecdenier@gmail.com>
2024-03-14 21:50:59 -0500

    Created initial prompt and first function based on answers.

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Connects to the database
+// Connects to the database.
 const db = mysql.createConnection(
   {
     host: "localhost",
@@ -22,13 +22,53 @@ const db = mysql.createConnection(
   console.log(`Connected to the employee_tracker_db database.`)
 );
 
-function viewDepartments(answers) {
+// Requests to see all departments.
+async function viewdepartments() {
+  db.query("SELECT id, name FROM department", function (err, results) {
+    console.log("\nID\tName\n---\t----------");
+    for (let i = 0; i < results.length; i++) {
+      console.log(results[i].id + "\t" + results[i].name);
+    }
+    console.log(" ");
+    reinit();
+  });
+}
+
+// Requests to see all roles.
+async function viewroles() {
+  db.query("SELECT id, title, salary FROM role", function (err, results) {
+    console.table(results);
+    reinit();
+  });
+}
+
+// Selects a function based on answers to the prompt.
+function pickaction(answers) {
   if (answers.action === "View all departments") {
-    // Requests to see all departments.
-    db.query("SELECT * FROM department", function (err, results) {
-      console.log(results);
-    });
+    viewdepartments();
+  } else if (answers.action === "View all roles") {
+    viewroles();
   }
+}
+
+function reinit() {
+  inquirer
+    .prompt([
+      {
+        name: "reinit",
+        message: "Is there more you would like to do?",
+        type: "list",
+        choices: ["Yes", "No"],
+      },
+    ])
+    .then((answers) => {
+      if (answers.reinit === "Yes") {
+        init();
+      } else {
+        //TODO: Change this so it automatically exits for you.
+        console.log("Okay press 'control + c' to exit.");
+      }
+    });
 }
 
 // Asks the user for choices regarding logo creation and then runs all logo creation functions.
@@ -51,7 +91,7 @@ function init() {
       },
     ])
     .then((answers) => {
-      viewDepartments(answers);
+      pickaction(answers);
     });
 }
 
